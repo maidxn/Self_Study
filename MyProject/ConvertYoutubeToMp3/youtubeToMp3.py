1340a6d2461961fdee547dfa31b88f761f950fa0
dxmai <19521820@gm.uit.edu.vn>
2021-12-31 17:17:41 +0700

    Yeah, no more converter. I'm crying ;;-;;

--- MyProject/ConvertYoutubeToMp3/youtubeToMp3.py
+++ MyProject/ConvertYoutubeToMp3/youtubeToMp3.py
@@ -2,6 +2,10 @@ from pytube import YouTube
 import time
 
 
+#Maybe you've already known, the Youtube new version from 2022 is no longer to be get streams
+#By pytube :<<<
+
+
 def GetMp3File(arr_url, arr_name, pos):
     yt = YouTube(arr_url[pos])
     file = yt.streams.get_audio_only()
@@ -10,12 +14,12 @@ def GetMp3File(arr_url, arr_name, pos):
     print("Done!", name, arr_url[pos], pos)
 
 
-url_file = open(r'D:/Code/python/pythonProject/url.txt', "r")
+url_file = open(r'D:/Code/python/pythonProject/MyProject/ConvertYoutubeToMp3/url.txt', "r")
 url_arr = url_file.read()
 urls = url_arr.splitlines()
 url_file.close()
 
-name_file = open(r'D:/Code/python/pythonProject/name.txt', "r")
+name_file = open(r'D:/Code/python/pythonProject/MyProject/ConvertYoutubeToMp3/name.txt', "r")
 name_arr = name_file.read()
 names = name_arr.splitlines()
 name_file.close()
@@ -24,9 +28,10 @@ for item in range(len(urls)):
     GetMp3File(urls, names, item)
     time.sleep(5)
 
-# url = 'https://www.youtube.com/watch?v=uQo8mUEqkkE&list=PLDn9IICB2Wiv2JFCUgP9SPLikmeBax00U'
+# url = 'https://www.youtube.com/watch?v=3Uo0JAUWijM'
 # yt = YouTube(url)
-# print(yt.streams.filter(only_audio=True))
-# a = yt.streams.get_audio_only()
-# a.download()
-
+# print(yt.streams)
+# name = "HappyNewYear.mp3"
+# file = yt.streams.get_audio_only()
+# file.download(output_path=r'D:/Code/python/pythonProject/Music', filename=name)
+# print(file)
