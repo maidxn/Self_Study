11b4bda88439347a95d456804b9d0456d2b805c9
dxmai <19521820@gm.uit.edu.vn>
2022-08-13 15:42:54 +0700

    django blog project

---
+++ MyProject/Backend/blogproject/posts/views.py
@@ -0,0 +1,13 @@
+import imp
+from django.shortcuts import render
+from . import views
+from .models import Post
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})
+    
+def post(request, pk):
+    posts = Post.objects.get(id=pk)
+    return render(request, 'post.html', {'posts': posts})
