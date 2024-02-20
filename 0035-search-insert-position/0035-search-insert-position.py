0dcfd14e763cc1fb1870feeccb8ef6a1819917ba
Dang Xuan Mai <67418054+maidxn@users.noreply.github.com>
2024-02-20 16:31:33 +0700

    Time: 57 ms (19.16%), Space: 17.5 MB (48.37%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -4,11 +4,9 @@ class Solution:
             return len(nums)
         if nums[0] > target:
             return 0
+        
         left, right, mid = 0, len(nums) - 1, len(nums) // 2
-        print(mid)
         while left < right:
-            # print("left, right", left, right)
-            # print(nums[left:right+1])
             if nums[mid] == target:
                 return mid
             if target < nums[mid]:
@@ -16,10 +14,6 @@ class Solution:
             else: 
                 left = mid + 1
             mid = (right + left) // 2
-            # print("update", mid)
-            # print(left, right)
-        print("last")
-        print(left, right)
         if target <= nums[left]:
             return left
         return left + 1
