ea13c7f520f5eaef0b0152a2bea4395024fcf280
Dang Xuan Mai <67418054+maidxn@users.noreply.github.com>
2024-02-23 16:50:20 +0700

    Time: 42 ms (98.55%), Space: 16.6 MB (70.74%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -9,36 +9,48 @@ class Solution:
         res = head
         add = 0
         
-        while l1 != None and l2 != None:
-            val = (l1.val + l2.val + add) % 10
-            print("VAL", val)
-            add = 1 if l1.val + l2.val + add >= 10 else 0 
+#         while l1 != None and l2 != None:
+#             val = (l1.val + l2.val + add) % 10
+#             add = 1 if l1.val + l2.val + add >= 10 else 0 
+#             new = ListNode(val)
+#             res.next = new
+#             res = res.next
+
+#             l1 = l1.next
+#             l2 = l2.next
+            
+#         while l1 is not None:
+#             val = (l1.val + add) % 10
+#             add = 1 if l1.val + add >= 10 else 0
+#             new = ListNode(val)
+#             res.next = new
+#             res = res.next
+#             l1 = l1.next
+#         while l2 is not None:
+#             val = (l2.val + add) % 10
+#             add = 1 if l2.val + add >= 10 else 0
+#             new = ListNode(val)
+#             res.next = new
+#             res = res.next
+#             l2 = l2.next
+            
+#         if add:
+#             new = ListNode(1)
+#             res.next = new
+#             res = res.next
+        
+        while l1 is not None or l2 is not None or add != 0:
+            val1 = 0 if l1 is None else l1.val
+            val2 = 0 if l2 is None else l2.val
+            
+            val = (val1 + val2 + add) % 10
+            add = (val1 + val2 + add) // 10
             new = ListNode(val)
-            print(new)
             res.next = new
             res = res.next
             
-            l1 = l1.next
-            l2 = l2.next
-        while l1 is not None:
-            val = (l1.val + add) % 10
-            add = 1 if l1.val + add >= 10 else 0
-            new = ListNode(val)
-            res.next = new
-            res = res.next
-            l1 = l1.next
-        while l2 is not None:
-            val = (l2.val + add) % 10
-            add = 1 if l2.val + add >= 10 else 0
-            new = ListNode(val)
-            res.next = new
-            res = res.next
-            l2 = l2.next
-        print(add)
-        if add:
-            new = ListNode(1)
-            res.next = new
-            res = res.next
-        
+            l1 = l1.next if l1 is not None else None
+            l2 = l2.next if l2 is not None else None
+            
         return head.next
         
