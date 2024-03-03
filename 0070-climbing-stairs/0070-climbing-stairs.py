45c0f4bb8b4e24b544d46a38518c00e35bd92351
Dang Xuan Mai <67418054+maidxn@users.noreply.github.com>
2024-03-03 15:48:23 +0700

    Time: 24 ms (98.01%), Space: 16.6 MB (69.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,8 +3,17 @@ class Solution:
         
         if n <= 2:
             return n
-        dp = [0] * n
-        dp[0], dp[1] = 1, 2
+        # dp
+        # dp = [0] * n
+        # dp[0], dp[1] = 1, 2
+        # for i in range(2, n, 1):
+        #     dp[i] = dp[i - 1] + dp[i - 2]
+        # return dp[-1]
+        
+        # no dp
+        pre1, pre2 = 1, 2
         for i in range(2, n, 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[-1]
+            current = pre1 + pre2
+            pre1 = pre2
+            pre2 = current
+        return current
