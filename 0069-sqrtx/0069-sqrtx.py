8f03cbba42b7d8dd2fe4578d71e66bd341ba15a5
Dang Xuan Mai <67418054+maidxn@users.noreply.github.com>
2024-03-03 15:14:20 +0700

    Time: 39 ms (64.13%), Space: 16.6 MB (34.15%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,6 +1,19 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        res = 0
-        while (res + 1) * (res + 1) <= x:
-            res += 1
-        return res
+        # res = 0
+        # while (res + 1) * (res + 1) <= x:
+        #     res += 1
+        # return res
+    
+        # binary search
+        left, right = 0, x
+        while left <= right:
+            mid = (right + left) // 2
+            square = mid * mid
+            if square == x:
+                return mid
+            elif square < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
