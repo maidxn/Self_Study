a681d619d6d5d752ade26e03a01f48c88a7c4b51
Dang Xuan Mai <67418054+maidxn@users.noreply.github.com>
2024-03-05 16:55:53 +0700

    Time: 108 ms (98.80%), Space: 20.5 MB (80.20%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -3,7 +3,7 @@ class Solution:
         res = []
         intervals = sorted(intervals, key=lambda x: x [0])
         temp = intervals[0]
-        print(temp)
+
         for interval in intervals:
             if temp[0] > interval[1]:
                 res.append(interval)
@@ -13,9 +13,7 @@ class Solution:
                 temp = interval
             else:
                 temp = [min(temp[0], interval[0]), max(temp[1], interval[1])]
-                print("update")
-                print(temp)
-        print(res)
+
         res.append(temp)
         return res
         
