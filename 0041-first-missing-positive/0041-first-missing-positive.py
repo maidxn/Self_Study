5919f36c6ed7a627a325385b87ccb411d122489c
Dang Xuan Mai <67418054+maidxn@users.noreply.github.com>
2024-03-27 00:48:34 +0700

    Time: 296 ms (54.55%), Space: 30.3 MB (55.57%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -2,20 +2,29 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         nums.append(0)
         N = len(nums) 
-        
+
+
         for index in range(N):
             if nums[index] < 0 or nums[index] > N:
                 nums[index] = 0
-        check = nums[0]
-        
+        value = nums[0]
+
+                
         for index in range(N):
             if nums[index] > 0:
-                nums[nums[index]%N - 1] += N
-     
-        if nums[0] == check: return 1
-        
-        for index in range(N):
-            if nums[index] // N == 0:
-                return index + 1
+                update_index = nums[index] % N - 1
+                nums[update_index] += N
 
+        if value == nums[0]:
+            return 1 
+        # quên gòi :"> hết biết làm gòi
+        # cíuuuuu
+        # tới đây gòi sao nữa :<<<< à =))) lâu quá gòi, hơn gòi, lẻ 3 ngày nưa 
+        for index in range(N):
+            if nums[index] // N == 0: # =)))
+                return index + 1 
         return N
+        # tới đây gòi hổng biết làm anh iu :< chỉ đi :<<<
+        # thì nhớ làm như zị à :>> à hiểu gòi
+        # ơ???? đúng gòi mà baaaaa
+        # ủa sai chỗ nàooooooooooooo
