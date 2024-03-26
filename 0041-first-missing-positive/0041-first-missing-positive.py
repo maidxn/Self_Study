b0052f9ad01c3fe26d940097c65acf00fbc511a7
Dang Xuan Mai <67418054+maidxn@users.noreply.github.com>
2024-03-27 00:50:45 +0700

    Time: 304 ms (38.23%), Space: 30.3 MB (55.57%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -11,20 +11,14 @@ class Solution:
 
                 
         for index in range(N):
-            if nums[index] > 0:
+            if nums[index] > 0: 
                 update_index = nums[index] % N - 1
                 nums[update_index] += N
 
         if value == nums[0]:
             return 1 
-        # quên gòi :"> hết biết làm gòi
-        # cíuuuuu
-        # tới đây gòi sao nữa :<<<< à =))) lâu quá gòi, hơn gòi, lẻ 3 ngày nưa 
+
         for index in range(N):
             if nums[index] // N == 0: # =)))
                 return index + 1 
         return N
-        # tới đây gòi hổng biết làm anh iu :< chỉ đi :<<<
-        # thì nhớ làm như zị à :>> à hiểu gòi
-        # ơ???? đúng gòi mà baaaaa
-        # ủa sai chỗ nàooooooooooooo
